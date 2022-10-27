9a3d4cf64ea4b590e9e8fbe9da3f6dab65fe14f0
aurora-1606 <97395939+aurora-1606@users.noreply.github.com>
2022-10-27 23:02:58 +0530

    updates
    created a django server and function for homepage in views.py.Created an index.html file in templates.

--- webster=22/smiteshop/smiteshop/settings.py
+++ webster=22/smiteshop/smiteshop/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'smiteshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
