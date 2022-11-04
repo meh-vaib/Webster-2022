18a7d29e5ff3449534b59d05036ef6361b158ab1
aurora-1606 <97395939+aurora-1606@users.noreply.github.com>
2022-11-04 22:53:04 +0530

    vendor shop apps added

--- webster=22/smiteshop/shop/urls.py
+++ webster=22/smiteshop/shop/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
 path('', views.index, name="ShopHome"),
 path("about/", views.about, name="AboutUs"),
 path("contact/", views.contact, name="ContactUs"),
+<<<<<<< Updated upstream
+path("tracker/", views.tracker, name="TrackingStatus"),
+>>>>>>> Stashed changes
 path("search/", views.search, name="Search"),
 path("productview/", views.productView, name="ProductView"),
 path("checkout/", views.checkout, name="Checkout"),
