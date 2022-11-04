b0a614745dadb613809281afcee917e9560cc5a9
agrawal-i <agraw.ishika@gmail.com>
2022-11-05 01:37:17 +0530

    Media Directory added+models modified

--- webster=22/smiteshop/shop/urls.py
+++ webster=22/smiteshop/shop/urls.py
@@ -5,13 +5,8 @@ urlpatterns = [
 path('', views.index, name="ShopHome"),
 path("about/", views.about, name="AboutUs"),
 path("contact/", views.contact, name="ContactUs"),
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 path("tracker/", views.tracker, name="TrackingStatus"),
->>>>>>> Stashed changes
-=======
 path("tracker/", views.tracker, name="TrackingStatus"),
->>>>>>> Stashed changes
 path("search/", views.search, name="Search"),
 path("productview/", views.productView, name="ProductView"),
 path("checkout/", views.checkout, name="Checkout"),
