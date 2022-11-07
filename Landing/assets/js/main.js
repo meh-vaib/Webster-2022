05c4f9c0df9b7575d25aafc76deee95ac968760e
Vaibhav Mehrotra <mehrotravaibhav09@gmail.com>
2022-11-08 00:36:13 +0530

    Carousel Added

--- Landing/assets/js/main.js
+++ Landing/assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Bell - v4.8.1
-* Template URL: https://bootstrapmade.com/bell-free-bootstrap-4-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
@@ -171,3 +166,61 @@
   new PureCounter();
 
 })()
+
+// ADDING SLIDER!!
+
+var responsiveSlider = function() {
+
+  var slider = document.getElementById("slider");
+  var sliderWidth = slider.offsetWidth;
+  var slideList = document.getElementById("slideWrap");
+  var count = 1;
+  var items = slideList.querySelectorAll("li").length;
+  var prev = document.getElementById("prev");
+  var next = document.getElementById("next");
+  
+  window.addEventListener('resize', function() {
+    sliderWidth = slider.offsetWidth;
+  });
+  
+  var prevSlide = function() {
+    if(count > 1) {
+      count = count - 2;
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    }
+    else if(count = 1) {
+      count = items - 1;
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    }
+  };
+  
+  var nextSlide = function() {
+    if(count < items) {
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    }
+    else if(count = items) {
+      slideList.style.left = "0px";
+      count = 1;
+    }
+  };
+  
+  next.addEventListener("click", function() {
+    nextSlide();
+  });
+  
+  prev.addEventListener("click", function() {
+    prevSlide();
+  });
+  
+  setInterval(function() {
+    nextSlide()
+  }, 8000);
+  
+  };
+  
+  window.onload = function() {
+  responsiveSlider();  
+  }
